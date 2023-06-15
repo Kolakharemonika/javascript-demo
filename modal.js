1256015a8cf6813d51b4a77828082b864d5bec86
Kolakharemonika <kolakharemonika@gmail.com>
2023-06-15 10:22:21 +0530

    udemy modal show screen

--- modal.js
+++ modal.js
@@ -1,16 +1,36 @@
-document.querySelector(".displaymodal").style.opacity = 0;
+// document.querySelector("button").addEventListener("click", function () {
+//   document.querySelector(".modal").style.opacity = 1;
+//   document.querySelector(".modal").style.zIndex = 100;
+//   document.querySelector(".overlay").style.opacity = 0.2;
+//   document.querySelector(".ovrlay").style.zIndex = 50;
+// });
+// document.querySelector(".close-modal").addEventListener("click", () => {
+//   document.querySelector(".modal").style.opacity = 0;
+//   document.querySelector(".ovrlay").style.opacity = 1;
+// });
+// document.querySelector("overlay").addEventListener("click", () => {
+//   document.querySelector(".modal").style.opacity = 0;
+//   document.querySelector(".ovrlay").style.opacity = 1;
+// });
 
-document.querySelector("button").addEventListener("click", function () {
-  document.querySelector(".displaymodal").style.opacity = 1;
-  document.querySelector(".displaymodal").style.zIndex = 100;
-  document.querySelector(".container").style.opacity = 0.2;
-  document.querySelector(".container").style.zIndex = 50;
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClose = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+btnOpenModal.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
 });
-document.querySelector(".close").addEventListener("click", () => {
-  document.querySelector(".displaymodal").style.opacity = 0;
-  document.querySelector(".container").style.opacity = 1;
-});
-document.querySelector("section").addEventListener("click", () => {
-  document.querySelector(".displaymodal").style.opacity = 0;
-  document.querySelector(".container").style.opacity = 1;
-});
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
