c04a1819cf361be5b2e37aa457a043933cdf78f0
Kolakharemonika <kolakharemonika@gmail.com>
2023-06-15 17:05:08 +0530

    udemy logic for pig-game

--- pig-game.js
+++ pig-game.js
@@ -3,62 +3,48 @@ const btnRollDice = document.querySelector(".btn-dice");
 const btnNewGame = document.querySelector(".btn-game");
 
 const numberDisplay = document.querySelector(".dice-number");
-
+const dice = document.querySelector("#dice");
 const player1 = document.querySelector(".player1");
 const player2 = document.querySelector(".player2");
+
 const player1currentscore = document.querySelector(".current-score-p1");
 const player2currentscore = document.querySelector(".current-score-p2");
 const player1finaldisplay = document.querySelector(".dice-span-p1");
 const player2finaldisplay = document.querySelector(".dice-span-p2");
 
 let activePlayer = 0;
+let currentscore = 0;
+let scores = [0, 0];
+let playing = true;
 player1.classList.add("active");
 
 let diceNumber;
 function generateDice() {
-  diceNumber = Number(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+  // diceNumber = Number(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+  diceNumber = Number(Math.trunc(Math.random() * 6) + 1);
 }
 
 function activate() {
-  activePlayer = !activePlayer;
-  if (activePlayer) {
-    player2.classList.add("active");
-    player1.classList.remove("active");
-  } else {
-    player1.classList.add("active");
-    player2.classList.remove("active");
-  }
-}
-
-function calculateScore(finaldisplay, currentscore) {
-  let score = Number(finaldisplay.innerHTML) + Number(currentscore.innerHTML);
-  finaldisplay.innerHTML = score;
-
-  if (Number(score) > 100) {
-    prompt(`winner is player ${activePlayer + 1}`);
-  }
-  currentscore.innerHTML = 0;
+  document.querySelector(`.current-score-p${activePlayer + 1}`).textContent = 0;
+  currentscore = 0;
+  activePlayer = activePlayer ? 0 : 1;
+  player1.classList.toggle("active");
+  player2.classList.toggle("active");
 }
 
 btnRollDice.addEventListener("click", () => {
-  generateDice();
-  numberDisplay.innerHTML = diceNumber;
-  numberDisplay.classList.remove("hidden");
+  if (playing) {
+    generateDice();
+    dice.src = `img/inverted-dice-${diceNumber}.svg`;
+    numberDisplay.classList.remove("hidden");
 
-  if (diceNumber != 1) {
-    if (activePlayer) {
-      player2currentscore.innerHTML =
-        Number(player2currentscore.innerHTML) + diceNumber;
+    if (diceNumber != 1) {
+      currentscore = currentscore + diceNumber;
+      document.querySelector(
+        `.current-score-p${activePlayer + 1}`
+      ).textContent = currentscore;
     } else {
-      player1currentscore.innerHTML =
-        Number(player1currentscore.innerHTML) + diceNumber;
-    }
-  } else {
-    activate();
-    if (activePlayer) {
-      player1currentscore.innerHTML = 0;
-    } else {
-      player2currentscore.innerHTML = 0;
+      activate();
     }
   }
 });
@@ -66,6 +52,9 @@ btnRollDice.addEventListener("click", () => {
 //Event listerners
 btnNewGame.addEventListener("click", () => {
   activePlayer = 0;
+  currentscore = 0;
+  scores = [0, 0];
+  playing = true;
   player1.classList.add("active");
   numberDisplay.classList.add("hidden");
 
@@ -76,13 +65,22 @@ btnNewGame.addEventListener("click", () => {
 });
 
 btnHold.addEventListener("click", () => {
-  numberDisplay.classList.add("hidden");
+  if (playing) {
+    numberDisplay.classList.add("hidden");
 
-  activate();
+    scores[activePlayer] = scores[activePlayer] + currentscore;
+    document.querySelector(`.dice-span-p${activePlayer + 1}`).textContent =
+      scores[activePlayer];
 
-  if (activePlayer) {
-    calculateScore(player1finaldisplay, player1currentscore);
-  } else {
-    calculateScore(player2finaldisplay, player2currentscore);
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      document
+        .querySelector(`.player${activePlayer + 1}`)
+        .classList.remove("active");
+      document
+        .querySelector(`.player${activePlayer + 1}`)
+        .classList.add("winner");
+    }
+    activate();
   }
 });
