46f8cf6bca1312d080d069fade65d93c54b051ef
Kolakharemonika <kolakharemonika@gmail.com>
2023-06-15 10:55:31 +0530

    ESC functionality, event keydown

--- modal.js
+++ modal.js
@@ -34,3 +34,7 @@ const closeModal = () => {
 
 btnClose.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
