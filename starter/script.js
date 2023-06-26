08cecb39e2ef63318a0881eab6c7af26658d2bcb
Kolakharemonika <kolakharemonika@gmail.com>
2023-06-26 10:47:15 +0530

    DOM createElement

--- starter/script.js
+++ starter/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+///////////////////////////////////////// Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +18,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +30,41 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+//entire element select then 
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header')
+//NodeLists
+const allSections = document.querySelectorAll('.section')
+console.log(allSections);
+
+document.getElementById('section--1')
+//html collections (if we deleted  from browser screen it will not shown entry )
+const allButtons = document.getElementsByTagName('button')
+console.log(allButtons);
+
+document.getElementsByClassName('btn');
+
+//creating and inserting elements
+//.insertAdjacementHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookied for improved functionallity and analytics. <button class="btn btn--close-cookie">Got it</button>'
+header.prepend(message); //top of header
+//can present only one either append or prepend -solution below
+// header.append(message); //bottom of header
+
+// header.append(message.cloneNode(true)); //can use same time two place
+
+header.before(message); //display top of header
+header.after(message); //disply bottom of header 
+
+//Delete element 
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  // message.remove(); //to remove div 
+  message.parentElement.removeChild(message); //anather method for removing child
+})
