dbc844da3bd413bec466e42b9d33681c9cfddbe4
Kolakharemonika <kolakharemonika@gmail.com>
2023-07-03 16:28:31 +0530

    callback hell example

--- oop/script.js
+++ oop/script.js
@@ -146,3 +146,16 @@ const getCountryAndNeighbour = (countryName) => {
 
 getCountryAndNeighbour('india');
 
+//callback hell example
+setTimeout(() => {
+    console.log('1 second passed');
+    setTimeout(() => {
+        console.log('2 seconds passed');
+        setTimeout(() => {
+            console.log('3 second passed');
+            setTimeout(() => {
+                console.log('4 second passed');
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
