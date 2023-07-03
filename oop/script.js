4f082598fe68610c649c8bf065bbbadcf6cdf041
Kolakharemonika <kolakharemonika@gmail.com>
2023-07-03 15:41:26 +0530

    first AJAX call,  XMLHttpRequest API coubtry info

--- oop/script.js
+++ oop/script.js
@@ -34,13 +34,13 @@ const Person = function (firstname, birthyear) {
 // console.log(new Person('Monika', 1997)); // {} if this.firstname = firstname; not defined
 const mk = new Person('Monika', 1997);
 const mkk = new Person('Monikak', 2001);
-console.log(mk, mkk, mk instanceof Person);
+// console.log(mk, mkk, mk instanceof Person);
 
 //prototypes
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
-    console.log((2037 - this.birthyear));
+    // console.log((2037 - this.birthyear));
 }
 
 mk.calcAge();
@@ -60,13 +60,58 @@ mk.calcAge();
  */
 
 //asynchronous
-const img = document.querySelector('.dog');
-img.src = '/img/inverted-dice-6.svg';
-img.addEventListener('load', function () {
-    img.classList.add('fadeIn'); //blur img
-});
+// const img = document.querySelector('.dog');
+// img.src = '/img/inverted-dice-6.svg';
+// img.addEventListener('load', function () {
+//     img.classList.add('fadeIn'); //blur img
+// });
 
-document.querySelector('p').style.width = '300px';
+// document.querySelector('p').style.width = '300px';
 
 //search public APIS
 // https://countries-api-836d.onrender.com/countries/
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+
+const getCountryByname = (countryName) => {
+    const request = new XMLHttpRequest();
+    // https://restcountries.com/v3.1/name/{name} //https://restcountries.com/ website
+    request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+    request.send();
+
+    request.addEventListener('load', function () {
+        // const data = JSON.parse(this.responseText);
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        // Object.entries(data.currencies).filter(lang => console.log(lang[1].name));
+        // const currency = Object.values(data.currencies)[0].name;
+        const { name, symbol } = Object.values(data.currencies)[0];
+        // console.log(name, symbol);
+
+        let langs = []
+        // Object.entries(data?.languages).filter(lang => langs.push(lang[1]));
+        // Object.values(data.languages).join(' ');
+
+        const countryEl = document.querySelector('.countries')
+        const html = `<article class="country">
+             <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+         <p class="country__row"><span>🗣️</span>${Object.values(data.languages).join(' ')}</p>
+          <p class="country__row"><span>💰</span>${name} : ${symbol}</p>
+        </div>  </article>`;
+        countryEl.insertAdjacentHTML('beforeend', html)
+        countryEl.style.opacity = 1;
+    })
+}
+
+
+getCountryByname('india');
+getCountryByname('usa');
+getCountryByname('pakistan');
+getCountryByname('nepal');
