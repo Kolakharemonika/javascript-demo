cc50dff6d0812f2bf8c73018f0b3f322e2cdfbbb
Kolakharemonika <kolakharemonika@gmail.com>
2023-07-07 15:38:47 +0530

    import function, values, module as object,

--- exportedModule.js
+++ exportedModule.js
@@ -1,1 +1,21 @@
 console.log('this module is exporting');
+const shippigCost = 10; //this is private 
+const cart = [];
+
+//simple function
+// const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`This ${quantity} ${product} added to Cart`);
+// }
+
+//we can import this function into anather module
+export const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(` ${quantity} ${product} added to Cart`);
+}
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+//we can also export this 
+export { totalPrice as price, totalQuantity }
--- importedModule.js
+++ importedModule.js
@@ -1,4 +1,25 @@
 
-import './exportedModule.js';
+import { addToCart, price, totalQuantity as tq } from "./exportedModule.js";
+
+//taking everything as object 
+import * as ShoppingCart from './exportedModule.js'
 
 console.log('importing module');
+
+addToCart('burgurs', 66); //function
+//same
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.price);
+console.log(ShoppingCart.totalQuantity);
+
+console.log(ShoppingCart);/**addToCart:(...) price:(...) totalQuantity:(...) */
+
+
+/**
+ * this module is exporting
+importedModule.js:4 importing module
+exportedModule.js:13  66 burgurs added to Cart
+ */
+
+// console.log(totalPrice, totalQuantity, 'imported from other module');
+console.log(price, tq, 'imported from other module'); //237 23 'imported from other module'
